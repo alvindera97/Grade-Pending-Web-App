21de315296d62528e70129753bcd6a7a6b33bc90
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-11-07 16:09:12 +0100

    Add Status Code Check To Login Page Functional Tests

--- home/tests/unit_tests/test_login_page.py
+++ home/tests/unit_tests/test_login_page.py
@@ -4,4 +4,5 @@ from .main import Main, site_main_title
 class LoginPageTest(Main):
     def test_login_page_loads(self):
         response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
         self.assertIn(site_main_title, response.content.title().decode())
