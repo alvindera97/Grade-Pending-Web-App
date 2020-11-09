817a78344b728e769ccd923734b092c08e6e538b
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-11-09 19:48:19 +0100

    Add FT To Test Link Pointing To Registration Page From Login Page

--- home/tests/functional_tests/test_login_page.py
+++ home/tests/functional_tests/test_login_page.py
@@ -16,3 +16,9 @@ class TestLoginPage(Main):
     def test_login_page_displays_at_login_url(self):
         self.get_login_page()
         self.assertIn(site_main_title, self.browser.title)
+
+    def test_already_have_an_account_link(self):
+        self.get_login_page()
+        already_have_an_account_link = self.browser.find_element_by_id("if-account-link")
+        already_have_an_account_link.click()
+        self.assertEqual(self.browser.current_url, self.live_server_url + '/signup/')
