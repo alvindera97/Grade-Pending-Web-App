3ed60e496f588048cf70aee007b3f1235f94b762
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-11-09 19:46:22 +0100

    Add Method For Loading Login Page (@ login FT)
    Method allows for reduction of source code length (i.e. eliminates need
    to explicitly call self.browser.get())

--- home/tests/functional_tests/test_login_page.py
+++ home/tests/functional_tests/test_login_page.py
@@ -10,6 +10,9 @@ class TestLoginPage(Main):
         super(TestLoginPage, self).setUp()
         self.live_login_url = self.live_server_url + '/login/'
 
-    def test_login_page_displays_at_login_url(self):
+    def get_login_page(self):
         self.browser.get(self.live_login_url)
+
+    def test_login_page_displays_at_login_url(self):
+        self.get_login_page()
         self.assertIn(site_main_title, self.browser.title)
