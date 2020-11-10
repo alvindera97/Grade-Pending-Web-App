774feba9e5cf166e334ec64337a6d0e708e80d57
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-11-10 18:30:54 +0100

    Set Up Home Page View Of Institutions Page

--- institutions/views.py
+++ institutions/views.py
@@ -1,3 +1,10 @@
+"""Views module for the Institutions app"""
+
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def institutions_home(request):
+    """Institutions Home Page View Function"""
+    return render(request, 'home/institutions_home.html', {})
