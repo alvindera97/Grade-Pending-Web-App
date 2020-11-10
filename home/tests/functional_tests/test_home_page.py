3a295acfccf5fbdf634ad91a44ef33aa47e6d9fc
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-11-10 16:43:20 +0100

    Add User Story To Home Page FT

--- home/tests/functional_tests/test_home_page.py
+++ home/tests/functional_tests/test_home_page.py
@@ -14,16 +14,28 @@ class TestHomePage(Main):
 
     def test_home_page_title(self):
         """Home page title test"""
+        # James hears of this new website, it's known as a universal grade pending calculator
+        # He opens the page and he sees that the page title self-explains the purpose of the website
+        # The title of the page (or home page) reads:
+        # "Grade Pending Calculator | Calculate Your Gp And Cgpa In Seconds"
         self.assertEqual(self.browser.title, site_main_title)
 
     def test_login_button(self):
         """Test if the home page button is functional"""
+        # James is however confronted with 3 buttons that clearly spell out "Login", "Register" and "Institutions"
         home_page_button = self.browser.find_element_by_id('login-button')
+        # He decides to click on the loin button
         home_page_button.click()
+        # He can see from the login button that he is being directed to the login page because of the trailing
+        # /login/ in the browser URL.
         self.assertEqual(self.browser.current_url, self.live_server_url + '/login/')
 
     def test_register_button(self):
         """Test if the home page button is functional"""
+        # James is now back at the home page and proceeds to check the 'register button'
         home_page_button = self.browser.find_element_by_id('register-button')
+        # He clicks the register button
         home_page_button.click()
+        # After clicking the button, he is directed to the sign up page as he can see from the
+        # end of the URL pointing towards the sign up page '/signup/'
         self.assertEqual(self.browser.current_url, self.live_server_url + '/signup/')
--- home/tests/functional_tests/test_login_page.py
+++ home/tests/functional_tests/test_login_page.py
@@ -14,11 +14,20 @@ class TestLoginPage(Main):
         self.browser.get(self.live_login_url)
 
     def test_login_page_displays_at_login_url(self):
+        # Now James is at the login page
         self.get_login_page()
+        # He checks the title of the page just to ascertain that he is still on the same site
+        # He checks and sees that a large chunk of the login page's title is similar to the home page's title
         self.assertIn(site_main_title, self.browser.title)
+        # He also checks too see if the word "Login" is in the page's title
+        self.assertIn("Login", self.browser.title)
 
     def test_already_have_an_account_link(self):
+        # while on the login page
         self.get_login_page()
+        # james notices that there is an instruction below the login buttons
         already_have_an_account_link = self.browser.find_element_by_id("if-account-link")
+        # he clicks on the link
         already_have_an_account_link.click()
+        # the link redirects him to the registration page
         self.assertEqual(self.browser.current_url, self.live_server_url + '/signup/')
