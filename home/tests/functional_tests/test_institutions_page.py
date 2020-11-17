c8dab6fac8ca9c227ee48c5add1ec297c01a03d4
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-11-17 12:44:47 +0100

    Rename Test Method From 'institution'->'institutions' For Ease Of Readability

--- home/tests/functional_tests/test_institutions_page.py
+++ home/tests/functional_tests/test_institutions_page.py
@@ -7,7 +7,7 @@ from .main import Main
 
 class TestInstitutionsPage(Main):
     """Test suite for the institutions page as viewed from the homepage"""
-    def test_institution_page_loads(self):
+    def test_institutions_page_loads(self):
         """Test if institution page loads from Home Page"""
         self.browser.get(self.live_server_url)
         institutions_page_button = self.browser.find_element_by_id("institutions-button")
