2a9f538cf1b6e8903637ce4200b634d19d182781
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-11-22 13:44:18 +0100

    Add Test For Login Page Header And Add Reminder To Finish The Test

--- home/tests/functional_tests/test_login_page.py
+++ home/tests/functional_tests/test_login_page.py
@@ -1,6 +1,7 @@
 """
    Test suite responsible for testing unit functionalities of the login page
 """
+from typing import NoReturn
 
 from .main import Main, site_main_title
 
@@ -22,6 +23,13 @@ class TestLoginPage(Main):
         # He also checks too see if the word "Login" is in the page's title
         self.assertIn("Login", self.browser.title)
 
+    def test_Login_page_header(self) -> NoReturn:
+        """Tests if login page has header and if it's displayed"""
+        login_page_header = self.browser.find_element_by_id('login-page-header')
+        expected_text = "Welcome Back"
+        self.assertEqual(login_page_header, expected_text)
+        self.assertTrue(login_page_header.is_displayer())
+
     def test_already_have_an_account_link(self):
         """Tests for text asking if user already has account"""
         # while on the login page
@@ -31,3 +39,8 @@ class TestLoginPage(Main):
         already_have_an_account_link.click()
         # the link redirects him to the registration page
         self.assertEqual(self.browser.current_url, self.live_server_url + '/signup/')
+
+    def test_finish_the_test(self):
+        """The test would always fail. It is supposed to remind us to
+           FINISH WRITING THE TESTS"""
+        self.fail("Finish the test!")
